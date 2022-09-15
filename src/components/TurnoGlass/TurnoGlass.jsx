8b9f301b9c210a3c0d06c87ccc06e5ec5135cd56
GermanDobler <german.crack@hotmail.com>
2022-09-15 11:38:25 -0300

    :coffin:

--- src/components/TurnoGlass/TurnoGlass.jsx
+++ src/components/TurnoGlass/TurnoGlass.jsx
@@ -1,20 +1,49 @@
 import React from 'react'
 import './TurnoGlass.css'
 import calendar from '../../assets/calendar.svg'
+import punto from '../../assets/punto.svg'
+import clock from '../../assets/clock.svg'
+import Punto from './Punto'
+
+function TurnosGlassItem(props) {
+  return (
+    <div className='turno-glass__body__container'>
+      <div className='turno-glass__body__info'>
+        <Punto color={props.color}></Punto>
+        {/* <img src={punto} alt="icon" className='punto' /> */}
+        <div className='turno-glass__body__text'>
+          Recorrido Arte Europeo
+        </div>
+        <div className='hora__container'>
+          18:00 - 19:30
+          <div className='icon__clock'>
+            <img src={clock} alt="icon" />
+          </div>
+        </div>
+      </div>
+      <div className='turno-glass__body__nombre'>
+        Daniel Chavez
+      </div>
+    </div>
+  )
+}
+
 export default function TurnoGlass() {
   return (
-    <div className='container-turnoglass'>
-        <div className='header-turnoglass'>
-          <div className='titulo-turnoglass'>
-            Turnos Pendientes
-          </div>
-          <div className='icon-calendar'>
-          <img src={calendar} alt="icon" />
+    <div className='turno-glass__container'>
+      <div className='turno-glass__header'>
+        <div className='turno-glass__header__text'>
+          Turnos Pendientes
+          <div className='icon__calendar'>
+            <img src={calendar} alt="icon" />
           </div>
         </div>
-        <div className='body-turnoglass'>
-
-        </div>
+      </div>
+      <div className='turno-glass__body'>
+        <TurnosGlassItem color="#9F51DD"></TurnosGlassItem>
+        <TurnosGlassItem color="#E1B74A"></TurnosGlassItem>
+        <TurnosGlassItem color="#78F165"></TurnosGlassItem>
+      </div>
     </div>
   )
 }
